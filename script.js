208d9989956c7e381c9252c55b5b965c539827b6
Karton <antonkuzmenko.dev@gmail.com>
2012-12-14 22:43:46 +0200

    Morse code: russian alphabet & numbers & punctuation marks

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function Player(elem) {
-    this.dotSize = 200;
+function Player(elem, dotSize) {
+    this.dotSize = dotSize;
     this.dashSize = this.dotSize * 3;
-    this.space = this.dotSize * 7;
+    this.spaceSize = this.dotSize * 7;
+    this.queue = [];
+    this.queueCounter = 0;
+    this.queueSymbolCounter = 0;
 
     this.elem = elem;
 }
 
-Player.prototype.play = function(time) {
+Player.prototype.play = function() {
     this.elem.play();
 };
 
-Player.prototype.pause = function(time) {
+Player.prototype.pause = function() {
     this.elem.pause();
 };
 
@@ -18,17 +21,81 @@ Player.prototype.reset = function() {
     this.elem.currentTime = 0;
 };
 
-Player.prototype.interval = function(action, time) {
+Player.prototype.symbolToTime = function(symbol) {
+    symbol = symbol.split('');
+    var time = [];
+
+    for (var i = 0; i < symbol.length; i++) {
+        if (symbol[i] == '.') {
+            time.push(this.dotSize);
+        } else if (symbol[i] == '-') {
+            time.push(this.dashSize);
+        }
+    }
+
+    return time;
+};
+
+Player.prototype.addSymbolToQueue = function(symbol) {
+    if (symbol == ' ') {
+        this.queue.push({
+            type: 's',
+            time: [this.spaceSize]
+        });
+    } else {
+        this.queue.push({
+            type: 'l',
+            time: this.symbolToTime(symbol)
+        });
+    }
+};
+
+Player.prototype.run = function() {
+    if (this.queue[this.queueCounter] == undefined) {
+        this.queueCounter = 0;
+        this.queue = [];
+        return;
+    }
+
     var self = this;
 
-    self[action]();
-    setTimeout(function() {
-        if (action == 'play') {
-            self.pause();
-        } else {
-            self.play();
+    var type = this.queue[this.queueCounter].type;
+    var time = this.queue[this.queueCounter].time;
+    var currentTime = 0;
+
+    if (type == 's') {
+        this.pause();
+        this.queueCounter++;
+        currentTime = time[0];
+    } else if (type == 'l') {
+        if (time[this.queueSymbolCounter] == undefined) {
+            this.queueCounter++;
+
+            this.queueSymbolCounter = 0;
+            setTimeout(function() {
+                self.run();
+            }, this.dashSize);
+
+            return;
         }
-    }, time);
+
+        currentTime = time[this.queueSymbolCounter++];
+        console.log(currentTime);
+        this.play();
+    }
+
+    setTimeout(function() {
+        if (type == 's') {
+            self.run();
+        } else if (type == 'l') {
+            self.pause();
+            setTimeout(function() {
+                self.run();
+            }, self.dotSize);
+        }
+
+        self.reset();
+    }, currentTime);
 };
 
 function Morse(player, field, button, form) {
@@ -37,15 +104,66 @@ function Morse(player, field, button, form) {
     this.button = button;
     this.form = form;
     this.msg = '';
-    this.codeList = [];
+    this.codeList = [
+        {'.': '......'},
+        {1: '.----'},
+        {2: '..---'},
+        {3: '...--'},
+        {4: '....-'},
+        {5: '....'},
+        {6: '-....'},
+        {7: '--...'},
+        {8: '---..'},
+        {9: '----.'},
+        {0: '-----'},
+        {',': '.-.-.-'},
+        {';': '-.-.-'},
+        {':': '---...'},
+        {'?': '..--..'},
+        {'№': '-..-.'},
+        {'"': '.-..-.'},
+        {"'": '.----.'},
+        {'(': '-.--.-'},
+        {')': '-.--.-'},
+        {'!': '--..--'},
+        {'а': '.-'},
+        {'б': '-...'},
+        {'в': '.--'},
+        {'г': '--.'},
+        {'д': '-..'},
+        {'е': '.'},
+        {'ж': '...-'},
+        {'з': '--..'},
+        {'и': '..'},
+        {'й': '.---'},
+        {'к': '-.-'},
+        {'л': '.-..'},
+        {'м': '--'},
+        {'н': '-.'},
+        {'о': '---'},
+        {'п': '.--.'},
+        {'р': '.-.'},
+        {'с': '...'},
+        {'т': '-'},
+        {'у': '..-'},
+        {'ф': '..-.'},
+        {'х': '....'},
+        {'ц': '-.-.'},
+        {'ч': '---.'},
+        {'ш': '----'},
+        {'щ': '--.-'},
+        {'ы': ''},
+        {'ь': '-.--'},
+        {'э': '..-..'},
+        {'ю': '..--'},
+        {'я': '.-.-'}
+    ];
 
     this.events();
 }
 
 Morse.prototype.fillList = function() {
-    this.codeList = [
-        
-    ];
+
 };
 
 Morse.prototype.events = function() {
@@ -55,21 +173,49 @@ Morse.prototype.events = function() {
     };
 
     this.button.onclick = function(e) {
-        self.encode().run();
+        self.run();
     };
 };
 
-Morse.prototype.encode = function() {
+Morse.prototype.run = function() {
+    this.msg = this.field.value;
+    if (this.msg.length === 0) {
+        alert('Введите сообщение!');
+        return this;
+    }
+
+    this.msg = this.msg.toLowerCase();
+
+    var currentChar = '';
+    var encodedChar = '';
+    for (var msgCounter = 0; msgCounter < this.msg.length; msgCounter++) {
+        currentChar = this.msg.charAt(msgCounter);
+
+        for (var i = 0; i < this.codeList.length; i++) {
+            if (currentChar == ' ') {
+                encodedChar = currentChar;
+                break;
+            }
+
+            for (var key in this.codeList[i]) {
+                if (currentChar == key) {
+                    encodedChar = key.replace(key, this.codeList[i][key]);
+                    break;
+                }
+            }
+        }
+
+        this.player.addSymbolToQueue(encodedChar);
+    }
+
+    this.player.run();
+
     return this;
 };
 
-Morse.prototype.run = function() {
+var player = new Player(document.getElementById('player'), 130);
 
-};
-
-var player = new Player(document.getElementById('player'));
-
-var morse = new Morse(
+new Morse(
     player,
     document.getElementById('field'),
     document.getElementById('button'),
